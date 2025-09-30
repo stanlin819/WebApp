b951dcbdaf294040c8f84fd1ade25b7bb589d01a
stanlin819 <stanlin@iscom.com.tw>
2025-09-30 12:31:21 +0800

    transaction create

--- WebApp/Controllers/TransactionController.cs
+++ WebApp/Controllers/TransactionController.cs
@@ -65,37 +65,121 @@ public class TransactionController : Controller
     }
 
     [HttpPost]
-    public async Task<IActionResult> Create(Transaction transaction, string UserName, int userId = 0, int p =1)
+    public async Task<IActionResult> Create(Transaction transaction, string UserName, int userId = 0, int p = 1)
     {
-        Console.WriteLine("Transaction Id: " + transaction.Id);
-        var user = await _userservice.GetUserByUsername(UserName);
-        if (user == null)
+        if (Request.Headers["X-Requested-With"] == "XMLHttpRequest")
         {
-            TempData["Error"] = $"User {UserName} not found.";
-            return RedirectToAction("Create", new { uid = userId, page = p});
+            if (ModelState.IsValid)
+            {
+                var title = await _service.AddTransaction(transaction, userId);
+
+                return Json(new
+                {
+                    success = true,
+                    message = $"{title} created successfully.",
+                    userId = userId
+                });
+            }
+            ViewBag.UserName = UserName;
+            ViewBag.UserId = userId;
+            ViewBag.CurrentPage = p;
+            var model = new Transaction();
+            return PartialView("_CreateTransaction", model);
         }
-        if (transaction.Title == null)
+        else
         {
-            TempData["Error"] = "Please input Title";
-            return RedirectToAction("Create", new { uid = userId, page = p});
+            var user = await _userservice.GetUserByUsername(UserName);
+            if (user == null)
+            {
+                TempData["Error"] = $"User {UserName} not found.";
+                return RedirectToAction("Create", new { uid = userId, page = p });
+            }
+            if (transaction.Title == null)
+            {
+                TempData["Error"] = "Please input Title";
+                return RedirectToAction("Create", new { uid = userId, page = p });
+            }
+            if (transaction.Category == null)
+            {
+                TempData["Error"] = "Please select Category";
+                return RedirectToAction("Create", new { uid = userId, page = p });
+            }
+            var title = await _service.AddTransaction(transaction, user.Id);
+            TempData["Message"] = $"{title} created successfully.";
+
+            return RedirectToAction("Details", "User", new { id = user.Id, currentPage = p });
         }
-        if (transaction.Category == null)
-        {
-            TempData["Error"] = "Please select Category";
-            return RedirectToAction("Create", new { uid = userId, page = p});
-        }
-        Console.WriteLine("Category: " + transaction.Category);
-        var title = await _service.AddTransaction(transaction, user.Id);
-        TempData["Message"] = $"{title} created successfully.";
-        
-        return RedirectToAction("Details", "User", new { id = user.Id, currentPage = p});
+
     }
 
     public async Task<IActionResult> Delete(int Id, int userId, int page)
     {
-        await _service.DeleteTransaction(Id);
+        try
+        {
+            await _service.DeleteTransaction(Id);
 
-        return RedirectToAction("Details", "User", new { id = userId, currentPage = page });
+            if (Request.Headers["X-Requested-With"] == "XMLHttpRequest")
+            {
+                return Json(new { success = true });
+            }
+
+            TempData["Message"] = "Transaction deleted successfully.";
+            return RedirectToAction("Details", "User", new { id = userId, currentPage = page });
+        }
+        catch (Exception ex)
+        {
+            if (Request.Headers["X-Requested-With"] == "XMLHttpRequest")
+            {
+                return Json(new { success = false, message = "Failed to delete transaction" });
+            }
+
+            TempData["Error"] = "Failed to delete transaction.";
+            return RedirectToAction("Details", "User", new { id = userId, currentPage = page });
+        }
     }
 
+    [HttpGet]
+    public async Task<IActionResult> CreatePartial(int uid, int? page)
+    {
+        if (uid != 0)
+        {
+            var user = await _userservice.GetUser(uid);
+            ViewBag.UserName = user.Username;
+        }
+        else
+        {
+            ViewBag.UserName = null;
+        }
+        ViewBag.UserId = uid;
+        ViewBag.CurrentPage = page;
+        var model = new Transaction();
+        return PartialView("_CreateTransaction", model);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> TransactionTablePartial(int userId, int page = 1)
+    {
+        var user = await _userservice.GetUser(userId);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        var transactions = await _service.GetUserAllTransactions(userId);
+        var viewModels = transactions.Select(trans => new TransactionViewModel
+        {
+            Title = trans.Title,
+            Amount = trans.Amount,
+            Date = trans.Date,
+            Category = trans.Category,
+            IsIncome = trans.IsIncome,
+            UserName = user.Username,
+            Id = trans.Id
+        }).ToList();
+
+        ViewBag.UserId = userId;
+        ViewBag.currentPage = page;
+        ViewBag.ShowExportButton = true;
+        return PartialView("_TransactionsTable", viewModels);
+    }
 }
